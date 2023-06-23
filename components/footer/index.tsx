97540a689c9f948d9fe5ea66fd31d20f12a672d3
PawanJS <contactpawanjs@gmail.com>
2023-06-23 06:39:53 +0530

    completed desktop design homepage

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import * as footerImages from "@/assets/images/testimonials";
 import Link from "next/link";
+import footerLogo from "@/assets/images/footer/footer-logo.png";
 import { CtaArrow } from "../icons";
+import Image from "next/image";
 
 export const Footer = () => {
   return (
     <section
-      className="min-h-screen text-white relative"
+      className="min-h-screen relative py-[140px]"
       style={{ background: `url(${footerImages.pattern.src})` }}
     >
-      <div className="container mx-auto pt-[140px] pb-20 text-center">
-        <h2 className="text-[120px] leading-[130px]">
+      <div className="container mx-auto pb-36 text-center">
+        <h2 className="text-[120px] leading-[130px] text-white">
           Quality coffee sourced globally.
         </h2>
         <Link
@@ -22,6 +24,29 @@ export const Footer = () => {
             <CtaArrow />
           </span>
         </Link>
+      </div>
+      <div className="container mx-auto border-t border-t-[#2a4363] pt-16 flex items-center justify-center flex-col">
+        <Image src={footerLogo} alt="Footer logo" />
+        <div className="flex item-center mt-14 gap-5 text-sm">
+          <Link
+            className="hover:text-white transition-[color] duration-300"
+            href="/"
+          >
+            Terms & Conditions
+          </Link>
+          <Link
+            className="hover:text-white transition-[color] duration-300"
+            href="/"
+          >
+            Privacy Policy
+          </Link>
+          <Link
+            className="hover:text-white transition-[color] duration-300"
+            href="/"
+          >
+            ©Triple Shot Cafe 2023
+          </Link>
+        </div>
       </div>
     </section>
   );
