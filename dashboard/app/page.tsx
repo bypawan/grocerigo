4694a252bb3da7d60c07c7d1f1794bab7d9e5daa
PawanJS <hello.pawanjs@gmail.com>
2023-08-09 07:31:16 +0530

    Create Menuitems and sidebar items.
    - Menuitems are created into an array of objects.
    - Menu is dynamically genarated on adding items to array.

--- dashboard/app/page.tsx
+++ dashboard/app/page.tsx
@@ -1,17 +1,8 @@
-"use client";
-
-import { signOut } from "next-auth/react";
-
 import { Layout } from "@/layout";
-import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
   return (
     <main className="">
-      <div className="text-center">
-        <h1>Home</h1>
-        <Button onClick={() => signOut()}>Sign out</Button>{" "}
-      </div>
       <Layout />
     </main>
   );
