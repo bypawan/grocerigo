08843f69bf9fa83a7116d3485bb265a2860ee096
PawanJS <hello.pawanjs@gmail.com>
2023-08-11 08:44:06 +0530

    Created data table for listings
    - Added filters for data table
    - Added pagination plugin
    - Created seprate componets for operations

--- dashboard/app/store/(admin)/products/page.tsx
+++ dashboard/app/store/(admin)/products/page.tsx
@@ -2,22 +2,24 @@ import { getServerSession } from "next-auth";
 
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-export default async function ProductsPage() {
-  try {
-    const session = await getServerSession(authOptions);
-    const token = session.user.DATA.token;
+async function getProducts() {
+  const session = await getServerSession(authOptions);
+  const token = session.user.DATA.token;
 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/products`, {
-      method: "GET",
-      headers: { Authorization: `Bearer ${token}` },
-    });
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/products`, {
+    method: "GET",
+    headers: { Authorization: `Bearer ${token}` },
+  });
 
-    const products = await res.json();
-
-    console.log(products);
-  } catch (error) {
-    console.log(error);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
   }
 
-  return <main className="">Product</main>;
+  return res.json();
+}
+
+export default async function ProductsPage() {
+  const products = await getProducts();
+
+  return <main className="">{JSON.stringify(products)}</main>;
 }
