b380d429d5fe9c496f1e7314b5e378af269b9b8b
PawanJS <hello.pawanjs@gmail.com>
2023-08-15 20:20:08 +0530

    Spacing issue in table
    - Removed spacing issue in products table

--- dashboard/layout/index.tsx
+++ dashboard/layout/index.tsx
@@ -5,10 +5,10 @@ import { Header } from "./Header";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="min-h-screen grid grid-cols-[16rem,1fr] grid-rows-[6rem,1fr] gap-5 antialiased text-gray-800">
+    <div className="min-h-screen grid grid-cols-[16rem,1fr] grid-rows-[6rem,1fr] antialiased text-gray-800">
       <Sidebar />
       <Header />
-      <div className="col-start-2 pr-5">{children}</div>
+      <div className="col-start-2 p-5">{children}</div>
     </div>
   );
 };
